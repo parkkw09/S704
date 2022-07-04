d1f8eec597acd625bd1a733cd03e279de8f56971
parkkw09-mbp <parkkw09@gmail.com>
2022-07-04 13:26:48 +0900

    Initial Commit

---
+++ Shared/S704App.swift
@@ -0,0 +1,17 @@
+//
+//  S704App.swift
+//  Shared
+//
+//  Created by kwp-macbook-pro on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct S704App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
