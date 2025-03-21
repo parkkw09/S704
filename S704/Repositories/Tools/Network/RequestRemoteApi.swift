10b6c85e24072740d013f97b351a830be80308f7
parkkw09-mbp <parkkw09@gmail.com>
2025-03-21 10:54:44 +0900

    키 가져오기

--- S704/Repositories/Tools/Network/RequestRemoteApi.swift
+++ S704/Repositories/Tools/Network/RequestRemoteApi.swift
@@ -11,6 +11,11 @@ class RequestRemoteApi {
     private var content: Data?
 
     func requestCommand(Type type: RequestType, URL url: String, Body body: String = "", Response response: @escaping () -> Void) {
-        
+        print("requestCommand")
+
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "KEY") as? String else {
+            print("key is nil")
+            return
+        }
     }
 }
